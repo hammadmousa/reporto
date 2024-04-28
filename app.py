8f81a2bd94fee75f45e08dadcb1f43e1f9762133
Motaseam Yousef <motaseamsaher@gmailcom>
2024-04-28 20:59:14 +0300

    make it as layer

--- app.py
+++ app.py
@@ -34,92 +34,31 @@ def generate_content(img=None):
     load_dotenv()
     gemini_api_key = os.getenv('GEMINI_API_KEY')
     genai.configure(api_key=gemini_api_key)
+    config = genai.types.GenerationConfig(temperature=0)
     model = genai.GenerativeModel('gemini-1.0-pro-vision-latest')
     try:
         genai_client = genai.GenerativeModel('gemini-1.0-pro-vision-latest')
-        text_prompt = '''
-        You are an expert in interpreting medical examinations and providing optimal recommendations. 
-        You will analyze images containing medical examination data. 
-        Initially, explain the significance of the data presented. 
-        It is important to maintain a positive outlook throughout your analysis and mention the name (if it exists) to personalize and make it kind. 
-        Always ensure to include the advice of a specialist doctor for more detailed information and a comprehensive understanding. 
+        text_prompt = '''You are an expert in interpreting medical examinations. Here is the report result as text: '''
+        response = model.generate_content([text_prompt, img],generation_config=config)
+
+        # analysis model
+
+        ana_model = genai.GenerativeModel('gemini-1.5-pro-latest')
+        text_prompt_ana = f''' 
+        You are an expert in interpreting medical examinations. Here is the report result as text: "{response.text}" 
+        Please use the ranges from the previous report to conduct your analysis. 
+        Initially, explain the significance of the data, as points. 
+        Personalize the analysis by mentioning the individual's name (if available) and ensure the tone is kind. A
+        lways include the recommendation to consult a specialist doctor for more detailed information and a comprehensive understanding. 
         Please respond in simple, direct Arabic without using complex terms.
-        Finally say "مع خالص التقدير روبرتو"'''
-        response = model.generate_content([text_prompt, img])
-        return response.text
+        Finally say
+        "تحياتي روبرتو"'''
+        response_ana = ana_model.generate_content([text_prompt_ana],generation_config=config)
+
+        return response_ana.text
     except Exception as e:
         st.error("Failed to generate content: {}".format(e))
         return None
-
-def generate_audio(text, voice="Rachel", model="eleven_multilingual_v2"):
-    """
-    Generates an audio file from text using the Eleven Labs API.
-
-    Parameters:
-        text (str): The text to be converted into speech.
-        voice (str, optional): The name of the voice model to use. Defaults to "Rachel".
-        model (str, optional): The specific model of Eleven Labs to use. Defaults to "eleven_multilingual_v2".
-
-    Returns:
-        bytes: The audio data in byte format, or None if there's an error during generation.
-    """
-
-    load_dotenv()
-    elevenlabs_api_key = os.getenv('ELEVENLABS_API_KEY')
-    elevenlabs_client = ElevenLabs(api_key=elevenlabs_api_key)
-    
-    try:
-        audio_generator = elevenlabs_client.generate(text=text, voice=voice, model=model)
-        audio_data = b"".join(audio_generator)
-        return audio_data
-    except Exception as e:
-        print(f"Error generating audio: {e}")
-        return None
-
-def process_and_save_audio(input_text, output_directory):
-    """
-    Processes input text into separate audio files for each paragraph and saves them in the specified directory.
-
-    Parameters:
-        input_text (str): The text to be processed, split into segments based on double newlines.
-        output_directory (str): The path to the directory where audio files will be saved.
-
-    Returns:
-        None: Audio files are written directly to the file system.
-    """
-
-    segments = input_text.split('\n\n')
-    os.makedirs(output_directory, exist_ok=True)
-    for i, segment in enumerate(segments):
-        if segment.strip():
-            audio = generate_audio(segment)
-            if audio is None:
-                continue
-            file_name = f"{i+1:02}.mp3"
-            file_path = os.path.join(output_directory, file_name)
-            with open(file_path, "wb") as file:
-                file.write(audio)
-
-def combine_mp3_files(directory, output_filename):
-    """
-    Combines multiple MP3 files in a specified directory into a single MP3 file.
-
-    Parameters:
-        directory (str): The directory containing the MP3 files to combine.
-        output_filename (str): The name of the resulting combined MP3 file.
-
-    Returns:
-        None: The combined MP3 file is written directly to the file system and a message is printed indicating success.
-    """
-
-    files = sorted([f for f in os.listdir(directory) if f.endswith('.mp3')])
-    output_path = os.path.join(directory, output_filename)
-    with open(output_path, 'wb') as outfile:
-        for filename in files:
-            file_path = os.path.join(directory, filename)
-            with open(file_path, 'rb') as infile:
-                outfile.write(infile.read())
-    print("Files combined into:", output_path)
 
 # Streamlit app setup
 
@@ -154,23 +93,6 @@ def main():
             # Generate content based on text and image
             processed_text = generate_content(img)
             st.markdown(f"<div style='direction: rtl; text-align: right;'>{processed_text}</div>", unsafe_allow_html=True)  # Display the result from generate_content
-
-            # Process audio and combine files
-            output_dir = "./audio_reports"
-            process_and_save_audio(processed_text, output_dir)
-            combine_mp3_files(output_dir, "final_report.mp3")
-            st.success("Audio report generated! Check the './audio_reports' folder for the output.")
-
-            # Display the audio player
-            audio_file_path = os.path.join(output_dir, "final_report.mp3")
-            if os.path.isfile(audio_file_path):
-                audio_file = open(audio_file_path, "rb")
-                st.audio(audio_file.read(), format="audio/mp3")
-                audio_file.close()
-            else:
-                st.error("Audio file not found. Please ensure the audio generation process is correct.")
-        else:
-            st.error("Please provide an image.")
 
 if __name__ == "__main__":
     main()
